5859c662758da69db643b34af084f88b4f5cf28c
julianboyce1 <julian_boyce@hotmail.com>
2020-02-11 15:07:09 +0100

    add aws

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const app = express()
 
+// Packages for uploading a file to Amazon S3
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
 app.get("/aws_form", function(req, res) {
     res.sendFile("aws_form.html", { root: __dirname });
 })
